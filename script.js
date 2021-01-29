49ef949c1212356ae26ac25b374be641d652904f
Badhan112 <badhancsesastc112@gmail.com>
2021-01-30 03:41:53 +0600

    code organized

--- script.js
+++ script.js
@@ -7,13 +7,27 @@ function increase(numberOfTicket){
 
 //to decrease Input value
 function decrease(numberOfTicket){
-    //Prevent the value become negative because, Number of Ticket never be a negative value
+    //Prevent the value become negative. because, Number of Ticket never be a negative value
     if(numberOfTicket > 0){
         numberOfTicket--;
     }
     return numberOfTicket;
 }
 
+
+//to get value from Input Element
+function getValue(inputId){
+    const value = parseInt(document.getElementById(inputId).value);
+    return value;
+}
+
+
+//to set Text into HTML Element
+function setInnerText(id, text){
+    document.getElementById(id).innerText = text;
+}
+
+//variable declared outside functions because of re-use them [updateCost() & addBookBtnEventHandler()]
 let firstClassTicket = 0;
 let economyTicket = 0;
 let firstClassCost = 0;
@@ -24,8 +38,8 @@ let totalCost = 0;
 
 //function to Update Sub-total, Vat & Total Cost acording to Number of Ticket
 function updateCost(){
-    firstClassTicket = parseInt(document.getElementById("firstClass-numberOfTicket").value);
-    economyTicket = parseInt(document.getElementById("economy-numberOfTicket").value);
+    firstClassTicket = getValue("firstClass-numberOfTicket");
+    economyTicket = getValue("economy-numberOfTicket");
     firstClassCost = 150 * firstClassTicket;
     economyCost = 100 * economyTicket;
 
@@ -33,16 +47,16 @@ function updateCost(){
     vat = Math.round(subTotalCost * 0.1);
     totalCost = subTotalCost + vat;
 
-    document.getElementById("sub-total").innerText = subTotalCost;
-    document.getElementById("vat").innerText = vat;
-    document.getElementById("total").innerText = totalCost;
+    setInnerText("sub-total", subTotalCost);
+    setInnerText("vat", vat);
+    setInnerText("total", totalCost);
 }
 
 
 // Function to add Event Listener to plus & minus Button
 function addEventHandler(btnId, inputId, purpose){
     document.getElementById(btnId).addEventListener("click", function(){
-        let numberOfTicket = parseInt(document.getElementById(inputId).value);
+        let numberOfTicket = getValue(inputId);
         numberOfTicket = purpose(numberOfTicket);
         document.getElementById(inputId).value = numberOfTicket;
         updateCost();
@@ -59,12 +73,13 @@ function addBookBtnEventHandler(btnId){
         else{
             document.getElementById("ticket-confirmation-area").style.display = "flex";
             document.getElementById("ticket-booking-area").style.display = "none";
-            document.getElementById("confirm-total-cost").innerText = '$' + totalCost;
-            document.getElementById("confirm-firstClass-totalTicket").innerText = firstClassTicket;
-            document.getElementById("confirm-firstClass-totalCost").innerText = '$' + firstClassCost;
-            document.getElementById("confirm-economy-totalTicket").innerText = economyTicket;
-            document.getElementById("confirm-economy-totalCost").innerText = '$' + economyCost;
-            document.getElementById("confirm-total-vat").innerText = '$' + vat;
+
+            setInnerText("confirm-total-cost", totalCost);
+            setInnerText("confirm-firstClass-totalTicket", firstClassTicket);
+            setInnerText("confirm-firstClass-totalCost", firstClassCost);
+            setInnerText("confirm-economy-totalTicket", economyTicket);
+            setInnerText("confirm-economy-totalCost", economyCost);
+            setInnerText("confirm-total-vat", vat);
         }
     });
 }
